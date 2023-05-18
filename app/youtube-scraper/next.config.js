f79fa50f8d6b93d941092a777e01e6cb78a98a00
kaplantm <tonarie.k@gmail.com>
2023-05-17 20:14:05 -0400

    github pages action

--- app/youtube-scraper/next.config.js
+++ app/youtube-scraper/next.config.js
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
+  // https://github.com/gregrickaby/nextjs-github-pages
+  assetPrefix: isProd ? '/your-github-repo-name/' : '',
+  images: {
+    unoptimized: true,
+  },
+
+
   reactStrictMode: true,
   experimental: {
     swcPlugins: [
